876f87ff1890c942896f1cd39e03f449d48c43ee
sunisakc <sunisa_kan@gmail.com>
2017-12-27 15:24:29 +0100

    Submit

---
+++ GymClasses/GymClasses/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GymClasses.Startup))]
+namespace GymClasses
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
